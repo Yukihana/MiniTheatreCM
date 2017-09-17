e9ef047d11d177466ef27d47032e3938d5151557
MISAKI\Yuki <lilyflowerangel@gmail.com>
2017-09-17 07:42:31 +0530

    0.0.11.X

---
+++ site/views/item/view.html.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     MiniTheatre.Administrator
+ * @subpackage  com_minitheatrecm
+ *
+ * @copyright   CherrySoft-X 2017, MiniTheatre 2017
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @link        http://fb.me/LilyflowerAngel
+ */
+  
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HTML View class for the MiniTheatreCM_Item Component
+ *
+ * @since  0.0.1
+ */
+class MiniTheatreCMViewItem extends JViewLegacy
+{
+	/**
+	 * Display the Item view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		// Assign data to the view
+		$this->msg = JText::_('COM_MINITHEATRECM_ITEM_HEADING');
+
+		// Display the view
+		parent::display($tpl);
+	}
+}
